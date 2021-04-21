5c0ce2cb807151575c3fbe23cf35fa8c6ae3bd35
vinukaa <vinukawathsara@gmail.com>
2021-04-22 00:50:35 +0530

    added update function

--- Research/src/com/research/ResearchResource.java
+++ Research/src/com/research/ResearchResource.java
@@ -1,10 +1,14 @@
 package com.research;
 
+import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.JsonObject;
@@ -14,27 +18,48 @@ import com.research.model.Research;
 @Path("/research")
 public class ResearchResource {
 	Research research = new Research();
-	
+
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getResearch() {
-		return research.getResearch().toString();
+	public String getResearch(@QueryParam("research_id") String research_id) {
+		if (research_id==null) {
+			return research.getResearch().toString();
+		} else {
+			return research.getsingleResearch(Integer.parseInt(research_id)).toString();
+		}
 	}
-	
-	
+
+
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public String insertResearch(String researchJsonString) {
 		JsonObject researchJson =new  JsonParser().parse(researchJsonString).getAsJsonObject();
-		
+
 		return (research.insertResearch(researchJson.get("researcher_id").getAsInt(), researchJson.get("research_name").getAsString(), researchJson.get("description").getAsString(), researchJson.get("category").getAsString(), researchJson.get("expected_budget").getAsDouble())).toString();
 	}
-	
-	
-	
-	
-	
+
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String updateResearch(String researchJsonString) {
+		JsonObject researchJson =new  JsonParser().parse(researchJsonString).getAsJsonObject();
+
+		return (research.updateResearch(researchJson.get("research_id").getAsInt(),researchJson.get("researcher_id").getAsInt(), researchJson.get("research_name").getAsString(), researchJson.get("description").getAsString(), researchJson.get("category").getAsString(), researchJson.get("expected_budget").getAsDouble())).toString();
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String deleteResearch(String researchJsonString) {
+		JsonObject researchJson =new  JsonParser().parse(researchJsonString).getAsJsonObject();
+
+		return (research.deleteResearch(researchJson.get("research_id").getAsInt())).toString();
+	}
+
+
 }
--- Research/src/com/research/model/Research.java
+++ Research/src/com/research/model/Research.java
@@ -71,6 +71,7 @@ public class Research {
 		return result;
 	}
 
+	//insert research
 	public JsonObject insertResearch( int researcher_id , String name, String description,String category,double budget)
 	{
 		JsonObject result = null;
@@ -116,6 +117,150 @@ public class Research {
 		}
 		return result;
 	} 
+	
+	//update research
+	public JsonObject updateResearch( int research_id, int researcher_id , String name, String description,String category,double budget)
+	{
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for inserting.");
+			}
+
+
+			// create a prepared statement
+			String query = "UPDATE research SET researcher_id=?,research_name=?,description=?,category=?,expected_budget=? WHERE research_id=?";
+
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+
+			// binding values
+			preparedStmt.setInt(1, researcher_id);
+			preparedStmt.setString(2, name);
+			preparedStmt.setString(3, description);
+			preparedStmt.setString(4, category);
+			preparedStmt.setDouble(5,budget);
+			preparedStmt.setInt(6, research_id);
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			result = new JsonObject();
+			result.addProperty("SUCCESSFUL", "Updated successfully");
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION","Error while updating the item.");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	} 
+	
+	//delete research
+	public JsonObject deleteResearch( int research_id )
+	{
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for inserting.");
+			}
+
+
+			// create a prepared statement
+			String query = "delete from research where research_id=?";
+					 
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+
+			// binding values
+			preparedStmt.setInt(1, research_id);
+//			preparedStmt.setString(2, name);
+//			preparedStmt.setString(3, description);
+//			preparedStmt.setString(4, category);
+//			preparedStmt.setDouble(5,budget);
+//			preparedStmt.setInt(6, research_id);
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			result = new JsonObject();
+			result.addProperty("SUCCESSFUL", "Deleted successfully");
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION","Error while deleting the item.");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	} 
+	
+	
+	//get single record
+	public JsonObject getsingleResearch(int research_id) {
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for inserting."); 
+			}
+
+
+			// create a prepared statement
+			String query = " select * from research where research_id=?";
+
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			preparedStmt.setInt(1, research_id);
+			ResultSet rs = preparedStmt.executeQuery();
+			
+			JsonArray resultArray = new JsonArray();
+			while(rs.next()) {
+				JsonObject jsonRow = new JsonObject();
+				jsonRow.addProperty("research_id", rs.getInt("research_id") );
+				jsonRow.addProperty("researcher_id", rs.getInt("researcher_id") );
+				jsonRow.addProperty("research_name", rs.getString("research_name") );
+				jsonRow.addProperty("description", rs.getString("description") );
+				jsonRow.addProperty("category", rs.getString("category") );
+				jsonRow.addProperty("expected_budget", rs.getDouble("expected_budget") );
+				resultArray.add(jsonRow);
+				//System.out.println(jsonRow.toString());
+			}
+			result = new JsonObject();
+			result.add("research", resultArray);
+			
+			con.close();
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION", "Error occured while reading research");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	}
+	
+	
 
 
 }
