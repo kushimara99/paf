bf3d1782644cdb9cf92b545820b4e0aa5c05950b
kushi <kushani@codevus.com>
2021-04-22 01:47:47 +0530

    changes in delete and update

--- Gadgetbadget/src/com/gadgetbadget/ProductResource.java
+++ Gadgetbadget/src/com/gadgetbadget/ProductResource.java
@@ -1,8 +1,10 @@
 package com.gadgetbadget;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -30,6 +32,26 @@ public class ProductResource {
 	public String insertProduct(String productJsonString) {
 		JsonObject productJsonParsed = new JsonParser().parse(productJsonString).getAsJsonObject();
 		
-		return (product.insertProduct(productJsonParsed.get("researcher_id").getAsInt(), productJsonParsed.get("product_name").getAsString(), productJsonParsed.get("product_code").getAsString(), productJsonParsed.get("quantity").getAsInt(), productJsonParsed.get("product_price").getAsDouble())).toString();
+		return (product.insertProduct(productJsonParsed.get("researcher_id").getAsInt(),productJsonParsed.get("product_id").getAsInt(), productJsonParsed.get("product_name").getAsString(), productJsonParsed.get("product_code").getAsString(), productJsonParsed.get("quantity").getAsInt(), productJsonParsed.get("product_price").getAsDouble())).toString();
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String updateProduct(String productJsonString) {
+		JsonObject productJson =new  JsonParser().parse(productJsonString).getAsJsonObject();
+
+		return (product.updateProduct(productJson.get("id").getAsInt(),productJson.get("product_id").getAsInt(),productJson.get("researcher_id").getAsInt(), productJson.get("product_name").getAsString(), productJson.get("product_code").getAsString(), productJson.get("quantity").getAsInt(), productJson.get("product_price").getAsDouble())).toString();
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String deleteProduct(String productJsonString) {
+		JsonObject productJson =new  JsonParser().parse(productJsonString).getAsJsonObject();
+
+		return (product.deleteProduct(productJson.get("id").getAsInt())).toString();
 	}
 }
--- Gadgetbadget/src/com/gadgetbadget/model/Product.java
+++ Gadgetbadget/src/com/gadgetbadget/model/Product.java
@@ -85,7 +85,7 @@ public class Product {
 
  
 
-    public JsonObject insertProduct( int researcher_id , String name, String code,int qnty,double price)
+    public JsonObject insertProduct( int researcher_id,int product_id , String name, String code,int qnty,double price)
     {
         JsonObject result = null;
         try
@@ -105,8 +105,8 @@ public class Product {
 
             // create a prepared statement
             String query = " insert into product"+
-                    "(`researcher_id`,`product_name`,`product_code`,`quantity`,`product_price`)"
-                    + " values (?, ?, ?, ?, ?)";
+                    "(`researcher_id`,`product_id`,`product_name`,`product_code`,`quantity`,`product_price`)"
+                    + " values (?, ?, ?, ?, ?,?)";
 
  
 
@@ -118,10 +118,11 @@ public class Product {
 
             // binding values
             preparedStmt.setInt(1, researcher_id);
-            preparedStmt.setString(2, name);
-            preparedStmt.setString(3, code);
-            preparedStmt.setInt(4, qnty);
-            preparedStmt.setDouble(5,price);
+            preparedStmt.setInt(2, product_id);
+            preparedStmt.setString(3, name);
+            preparedStmt.setString(4, code);
+            preparedStmt.setInt(5, qnty);
+            preparedStmt.setDouble(6,price);
         
             // execute the statement
 
@@ -141,7 +142,112 @@ public class Product {
         }
         return result;
     } 
+    
+  //update research
+  	public JsonObject updateProduct( int id,int product_id, int researcher_id , String name, String code,int qnty,double price)
+  	{
+  		JsonObject result = null;
+  		try
+  		{
+  			Connection con = connect();
 
+  			if (con == null)
+  			{
+  				result = new JsonObject();
+  				result.addProperty("ERROR", "Error while connecting to the database for inserting.");
+  			}
+
+
+  			// create a prepared statement
+  			String query = "UPDATE product SET researcher_id=?,product_id=?,product_name=?,product_code=?,quantity=?,product_price=? WHERE id=?";
+
+
+  			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+
+  			// binding values
+  			
+  		  preparedStmt.setInt(1, researcher_id);
+  		 preparedStmt.setInt(2, product_id);
+          preparedStmt.setString(3, name);
+          preparedStmt.setString(4, code);
+          preparedStmt.setInt(5, qnty);
+          preparedStmt.setDouble(6,price);
+  			preparedStmt.setInt(7, id);
+  			
+  			// execute the statement
+  			int status = preparedStmt.executeUpdate();
+            con.close();
+           
+            result = new JsonObject();
+            if(status > 0) {
+                result.addProperty("SUCCESSFUL", "Updated successfully");
+               
+            }else {
+                result.addProperty("UNSUCCESSFUL", "Updated failed");
+            }
+  		
+  		}
+  		catch (Exception e)
+  		{
+  			result = new JsonObject();
+  			result.addProperty("EXCEPTION","Error while updating the item.");
+  			System.err.println(e.getMessage());
+  		}
+  		return result;
+  	} 
+  	
+  	//delete research
+  	public JsonObject deleteProduct( int id )
+  	{
+  		JsonObject result = null;
+  		try
+  		{
+  			Connection con = connect();
+
+  			if (con == null)
+  			{
+  				result = new JsonObject();
+  				result.addProperty("ERROR", "Error while connecting to the database for inserting.");
+  			}
+
+
+  			// create a prepared statement
+  			String query = "delete from product where product_id=?";
+  					 
+
+  			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+
+  			// binding values
+  			preparedStmt.setInt(1, id);
+//  			preparedStmt.setString(2, name);
+//  			preparedStmt.setString(3, description);
+//  			preparedStmt.setString(4, category);
+//  			preparedStmt.setDouble(5,budget);
+//  			preparedStmt.setInt(6, research_id);
+  			
+  			// execute the statement
+  			int status = preparedStmt.executeUpdate();
+            con.close();
+           
+            result = new JsonObject();
+            if(status > 0) {
+                result.addProperty("SUCCESSFUL", "Deleted successfully");
+               
+            }else {
+                result.addProperty("UNSUCCESSFUL", "Deleted failed");
+            }
+  			
+  		}
+  		catch (Exception e)
+  		{
+  			result = new JsonObject();
+  			result.addProperty("EXCEPTION","Error while deleting the item.");
+  			System.err.println(e.getMessage());
+  		}
+  		return result;
+  	}
  
 
 
